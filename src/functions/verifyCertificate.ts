7ae3f59dca8c257a7287a19852c3ca4816a82139
thiago <thiagosteps@gmail.com>
2021-11-04 14:48:37 +0000

    first commit

---
+++ src/functions/verifyCertificate.ts
@@ -0,0 +1,36 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+
+export const handle: APIGatewayProxyHandler = async (event) => {
+
+  const { id } = event.pathParameters;
+
+  const response = await document.query({
+    TableName: "users-certificate",
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": id
+    }
+  }).promise();
+
+  const userCertificate = response.Items[0];
+
+  if(userCertificate) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Certificado valido",
+        name: userCertificate.name,
+        url: "url certificado"
+      })
+    }
+  }
+
+  return {
+    statusCode: 400, 
+    body: JSON.stringify({
+      message: "Certificado Invalido"
+    }),
+  };
+
+}
